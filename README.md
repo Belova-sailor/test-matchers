8758cdcb6b7ca70a83bb949767b85149815e3149
Julia Belova <katishen@outlook.com>
2022-10-19 21:56:17 +0500

    Added test

---
+++ README.md
@@ -0,0 +1,3 @@
+# CI test demo
+
+[![Build status](https://ci.appveyor.com/api/projects/status/7k8799e4k5ckt52x?svg=true)](https://ci.appveyor.com/project/Belova-sailor/test-functions)
---
+++ src/js/__tests__/health.js
@@ -0,0 +1,14 @@
+import sortUsers from '../health';
+
+test('testing sort of data', () => {
+  const result = sortUsers([
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ]);
+  expect(result).toEqual([
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ]);
+});
